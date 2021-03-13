b18e87af38c04ac4c8089f9d4462e560357e8c2e
KilianTarb <kiliantarbett@gmail.com>
2021-03-14 01:59:35 +0800

    Use new classes

--- main.cpp
+++ main.cpp
@@ -10,26 +10,8 @@
 #include <vector>
 #include <filesystem>
 #include <unistd.h>
-#include <openssl/conf.h>
-#include <openssl/err.h>
-#include <openssl/aes.h>
 #include "indexfile.h"
-
-
-std::string genNewFileName(const int len, int seed) {
-    std::string tmp_s;
-    static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    srand((unsigned) seed);
-    tmp_s.reserve(len);
-    for (int i = 0; i < len; i++) 
-        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-
-    return tmp_s;
-}
-
+#include "AesEnc.h"
 
 const std::string OUTPUT_DIRECTORY_PATH = "./output";
 void createOutputDirectory() {
@@ -41,82 +23,30 @@ std::string createOutputFilePath(std::string fileName) {
     return OUTPUT_DIRECTORY_PATH + "/" + fileName;
 }
 
+void *print_file_callback(FileKey *file) {
+    printf("%s => %s\n", file->fileName, file->EncyptedFileName);
+    return NULL;
+}
 
 int main(int argc, char** argv) {
-    std::vector<FileKey> fileKeyPairs;
-
-    int fileNameSeedCounter = 10;
+    IndexFile index("index", "./output", 20);
 
     // Validate if args are directories
     for (int i = 1; i < argc; i++) {
-        printf("Finding directory %s...", argv[i]);
-        if (std::filesystem::is_directory(argv[i])) {
-            printf("Found\n");
-
-            // Iterate through files and directories
-            int fileCount = 0;
-            for(auto& p: std::filesystem::recursive_directory_iterator(argv[i])) {
-                if (p.is_regular_file()) {
-                    fileCount++;
-                    fileNameSeedCounter++;
-
-                    FileKey file;
-                    file.fileName = p.path().filename();
-                    file.filePath = p.path().relative_path();
-                    file.EncyptedFileName = genNewFileName(24, fileNameSeedCounter);
-
-                    fileKeyPairs.push_back(file);
-                }
-            }
-        } else { 
-            printf("Not found\n");
-            return 1;
-        }
+        printf("Finding directory %s...\n", argv[i]);
+        if (std::filesystem::is_directory(argv[i]))
+            index.AddDirectory(argv[i]);
+        else
+            printf("%s is not a directory, skipping...\n", argv[i]);
     }
 
     std::cout << "Encrypting..." << std::endl;
     createOutputDirectory();
 
-    /* 256 bit key */
-    AES_KEY key;
-    unsigned char ckey[] = "01234567890123456789012345678901";
-    AES_set_encrypt_key(ckey, 128, &key);
 
-    for (int i = 0; i < fileKeyPairs.size(); i++) {
-        std::FILE *source_file;
-        source_file = fopen(fileKeyPairs[i].filePath.c_str(), "r");
-
-        std::FILE *dest_file;
-        dest_file = fopen(createOutputFilePath(fileKeyPairs[i].EncyptedFileName).c_str(), "ab+");
-
-        int b_read, b_write; 
-        int current_read = 0;
-
-        fseek(source_file, 0, SEEK_END);
-        int total_read = ftell(source_file);
-        rewind(source_file);
-        
-        // File convertion summary
-        printf("%s => %s - %i bytes\n", 
-            fileKeyPairs[i].fileName.c_str(), 
-            fileKeyPairs[i].EncyptedFileName.c_str(), 
-            total_read
-        );
-
-        while (current_read < total_read) {
-            unsigned char *in_block = (unsigned char *)malloc(AES_BLOCK_SIZE);
-            unsigned char *out_block = (unsigned char *)malloc(AES_BLOCK_SIZE);
-
-            current_read += fread(in_block, 1, AES_BLOCK_SIZE, source_file);
-
-            AES_encrypt(in_block, out_block, &key);
-
-            b_write = fwrite(out_block, 1, AES_BLOCK_SIZE, dest_file);
-        }
-
-        fclose(source_file);
-        fclose(dest_file);
-    }
-
+    //AesEnc enc(&index, (unsigned char *)"123123123123123213", 128);
+    AesEnc enc(&index, (unsigned char *)"123123123123123213", 128);
+    enc.StartEncryption(NULL, &print_file_callback);
+    
     return 0;
 }
