fd3e227580b5661589c1844980b4563ea26ee8b3
KilianTarb <kiliantarbett@gmail.com>
2021-03-16 14:49:54 +0800

    Add creation of index file

--- main.cpp
+++ main.cpp
@@ -19,12 +19,24 @@ void createOutputDirectory() {
 }
 
 void *print_file_callback(FileKey *file) {
-    printf("%s => %s\n", file->fileName, file->EncyptedFileName);
+    cout << file->EncyptedFileName << " " << file->filePath << endl;
     return NULL;
 }
 
 int main(int argc, char** argv) {
     IndexFile index("index", "./output", 20);
+    
+    if (index.IndexFileExists()) {
+        printf("Index file already exists, aborting.");
+        return 1;
+    }
+    
+    createOutputDirectory();
+    if (!index.CreateIndexFile()) {
+        printf("Couldn't create index file (%s/%s), aborting.", index.Path.c_str(), index.Name.c_str());
+        return 1;
+    }
+    
 
     // Validate if args are directories
     for (int i = 1; i < argc; i++) {
@@ -36,7 +48,6 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "Encrypting..." << std::endl;
-    createOutputDirectory();
 
     AesEnc enc(&index, (unsigned char *)"123123123123123213", 128);
     enc.StartEncryption(NULL, &print_file_callback);
